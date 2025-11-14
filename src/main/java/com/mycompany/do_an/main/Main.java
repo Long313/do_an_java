8bdd60131ac30ed7e34b9d606856ee1e1d9b91fd
Long Tran <tranxuanlonga555@gmail.com>
2025-11-14 22:27:32 +0700

    change config pom file

--- src/main/java/com/mycompany/do_an/main/Main.java
+++ src/main/java/com/mycompany/do_an/main/Main.java
@@ -29,6 +29,7 @@ public class Main extends javax.swing.JFrame {
 
         panelBackground = new com.mycompany.do_an.swing.PanelBackground();
         header = new com.mycompany.do_an.component.Header();
+        menu1 = new com.mycompany.do_an.menu.Menu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
@@ -43,12 +44,16 @@ public class Main extends javax.swing.JFrame {
                 .addContainerGap()
                 .addComponent(header, javax.swing.GroupLayout.DEFAULT_SIZE, 753, Short.MAX_VALUE)
                 .addContainerGap())
+            .addGroup(panelBackgroundLayout.createSequentialGroup()
+                .addComponent(menu1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         panelBackgroundLayout.setVerticalGroup(
             panelBackgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(panelBackgroundLayout.createSequentialGroup()
                 .addComponent(header, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 320, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(menu1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -74,6 +79,7 @@ public class Main extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private com.mycompany.do_an.component.Header header;
+    private com.mycompany.do_an.menu.Menu menu1;
     private com.mycompany.do_an.swing.PanelBackground panelBackground;
     // End of variables declaration//GEN-END:variables
 }
---
+++ src/main/java/com/mycompany/do_an/menu/EventMenu.java
@@ -0,0 +1,6 @@
+package com.mycompany.do_an.menu;
+
+public interface EventMenu {
+
+    public void selectedMenu(int index);
+}
