72153a6bdfa32db05c8bf03908d714c0e0171f1e
Long Tran <tranxuanlonga555@gmail.com>
2025-12-07 22:17:36 +0700

    change link image

--- src/main/java/com/raven/theme/SystemTheme.java
+++
@@ -1,9 +0,0 @@
-package com.raven.theme;
-
-import java.awt.Color;
-
-public class SystemTheme {
-
-    //  Default theme color
-    public static Color mainColor = new Color(255, 102, 102);
-}
---
+++ src/main/java/com/theme/SystemTheme.java
@@ -0,0 +1,9 @@
+package com.theme;
+
+import java.awt.Color;
+
+public class SystemTheme {
+
+    //  Default theme color
+    public static Color mainColor = new Color(255, 102, 102);
+}
